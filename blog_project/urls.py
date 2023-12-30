231d02d0eb4d6a0120614ba03f6b4c5effef3680
mowuj <mowuj92.com>
2023-12-30 16:27:17 +0600

    post edit delete

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -1,9 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path,include
-
+from .import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home,name='homepage'),
     path('author/',include('author.urls')),
     path('profile/',include('profiles.urls')),
     path('post/',include('post.urls')),
